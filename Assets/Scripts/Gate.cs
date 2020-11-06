150d3a3b0dd29597d36bc02b449ec5b027465414
Obsessi0n <ruiccc100@gmail.com>
2020-11-06 12:40:49 +0000

    Created player pressure plate, gate, and a level blockout

---
+++ Assets/Scripts/Gate.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gate : MonoBehaviour
+{
+
+    public bool startOpen = false;
+
+    Vector3 startPosition;
+
+    private void Awake()
+    {
+        startPosition = transform.position;
+        if (startOpen)
+        {
+            OpenGate();
+        }
+    }
+    public void OpenGate()
+    {
+        transform.position = new Vector3(startPosition.x, startPosition.y + 3f, startPosition.z);
+    }
+
+    public void CloseGate()
+    {
+        transform.position = startPosition;
+    }
+}
---
+++ Assets/Scripts/PressurePlate.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressurePlate : MonoBehaviour
+{
+    public Gate gate;
+    public Sprite activatedSprite;
+    public Sprite deactivatedSprite;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+        if (collision.name == "Player" || collision.name == "PlayerHologram")
+        {
+            GetComponent<SpriteRenderer>().sprite = activatedSprite;
+            gate.OpenGate();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+       
+        if (collision.name == "Player" || collision.name == "PlayerHologram")
+        {
+            gate.CloseGate();
+            GetComponent<SpriteRenderer>().sprite = deactivatedSprite;
+        }
+    }
+
+}
